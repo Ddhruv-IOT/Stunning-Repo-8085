4fc4c0ac6b699d3601b30c0ff4fb6795246ddedd
Ddhruv <54676859+Ddhruv-IOT@users.noreply.github.com>
2022-02-24 20:55:44 +0530

    8085 Logical XOR operation

---
+++ Logical_XOR.asm
@@ -0,0 +1,12 @@
+# Simple 8085 program to demonstrate Logical XOR operation.
+
+# Loading data in Register A
+MVI A, 10H
+
+# Loading data in Register B
+MVI B, 20H 
+
+# Performing XOR operation using XRA Instruction.
+XRA B
+
+HLT
