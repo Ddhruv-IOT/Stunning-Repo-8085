401d26ec9eab74966666928a2b0291f21dab1a29
Ddhruv <54676859+Ddhruv-IOT@users.noreply.github.com>
2022-02-24 20:58:28 +0530

    8085 Logical AND operation

---
+++ Logical_AND.asm
@@ -0,0 +1,12 @@
+# Simple 8085 program to demonstrate Logical AND operation
+
+# Loading data in Register A
+MVI A, 10H
+
+# Loading data in Register B
+MVI B, 10H 
+
+# Performing AND operation using ANA Instruction.
+ANA B
+
+HLT
