97cb2a4d5c4d7d0feb1b993f29a0ba7cbf846813
Ddhruv <54676859+Ddhruv-IOT@users.noreply.github.com>
2022-02-27 22:47:17 +0530

    8085 RAR Operation
    Rotation Right with Carry

---
+++ right_rotation_carry_on_8bit_number.asm
@@ -0,0 +1,10 @@
+# Right rotation with carry on 8 Bit number
+# RAR -> Rotate Accumulator Right with Carry
+
+# Loading data in Register A
+MVI A, FFH
+
+# Rotating Right with Carry using RAR Instruction
+RAR
+
+HLT
