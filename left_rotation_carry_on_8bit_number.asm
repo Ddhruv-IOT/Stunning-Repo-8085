89f229a9cb19b5b361d1653e32fb213319da50f1
Ddhruv <54676859+Ddhruv-IOT@users.noreply.github.com>
2022-02-27 22:53:30 +0530

    8085 RAL Operation

---
+++ left_rotation_carry_on_8bit_number.asm
@@ -0,0 +1,10 @@
+# Left rotation with carry on 8 Bit number
+# RAL -> Rotate Accumulator Left with Carry
+
+# Loading data in Register A
+MVI A, FFH
+
+# Rotating Left with Carry using RAL Instruction
+RAL
+
+HLT
